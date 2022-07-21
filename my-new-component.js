f73e1a249c8d66bec601db5bf95eeca76a3e377c
Radoslav Sharapanov <rado@me.com>
2022-07-21 14:56:30 +0300

    ,

--- my-new-component.js
+++ my-new-component.js
@@ -1,8 +1,4 @@
 (function() {
-	let init = (host) => {
-		host.querySelectorAll(".my-new-component:not([data-ready])").forEach((el) => {
-			el.dataset.ready = true;
-		});
-	};
+	const init = host => host.querySelectorAll(".my-new-component:not([data-ready])").forEach(el => el.dataset.ready = true);
 	typeof registerComponent === "function" ? registerComponent("my-new-component", init) : init(document.body);
 })();
